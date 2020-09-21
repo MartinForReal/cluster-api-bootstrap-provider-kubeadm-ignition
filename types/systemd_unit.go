b96e8c55b66ff30b7ce86afe2ec2cdc460517f3d
mskj-zanghao <zanghao@mskj.com>
2020-09-21 15:34:44 +0800

    fix init master

--- types/systemd_unit.go
+++ types/systemd_unit.go
@@ -5,9 +5,9 @@ import (
 )
 
 const (
-	JoinControlPlaneUnitTemplate = "[Unit]\nDescription=init k8s\nAfter=docker.service\nRequires=extractk8s.service\nConditionFileExists=!/etc/kubernetes/kubelet.conf\n[Service]\nType=oneshot\nUser=root\nEnvironment=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/bin:/opt/bin/\nExecStart=/opt/bin/kubeadm join %s --config %s \n[Install]\nWantedBy=multi-user.target\n"
+	JoinControlPlaneUnitTemplate = "[Unit]\nDescription=init k8s\nAfter=cpk8s.service\nRequires=docker.service\nConditionFileExists=!/etc/kubernetes/kubelet.conf\n[Service]\nType=oneshot\nUser=root\nEnvironment=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/bin:/opt/bin/\nExecStart=/usr/local/bin/kubeadm join %s --config %s \n[Install]\nWantedBy=multi-user.target\n"
 	JoinWorkerUnitTemplate       = "[Unit]\nDescription=init k8s\nAfter=cpk8s.service\nRequires=docker.service\nConditionFileExists=!/etc/kubernetes/kubelet.conf\n[Service]\nType=oneshot\nUser=root\nEnvironment=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/bin:/opt/bin/\nExecStart=/usr/local/bin/kubeadm join %s --config %s \n[Install]\nWantedBy=multi-user.target\n"
-	InitUnitTemplate             = "[Unit]\nDescription=init k8s\nAfter=pullk8simages.service\nRequires=extractk8s.service\nConditionFileExists=!/etc/kubernetes/kubelet.conf\n[Service]\nType=oneshot\nUser=root\nEnvironment=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/bin:/opt/bin/\nExecStart=/opt/bin/kubeadm init %s --config %s \n[Install]\nWantedBy=multi-user.target\n"
+	InitUnitTemplate             = "[Unit]\nDescription=init k8s\nAfter=cpk8s.service\nRequires=docker.service\nConditionFileExists=!/etc/kubernetes/kubelet.conf\n[Service]\nType=oneshot\nUser=root\nEnvironment=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/bin:/opt/bin/\nExecStart=/usr/local/bin/kubeadm init %s --config %s \n[Install]\nWantedBy=multi-user.target\n"
 	KubeadmConfigPath            = "/etc/kubernetes/kubeadm.yaml"
 )
 
