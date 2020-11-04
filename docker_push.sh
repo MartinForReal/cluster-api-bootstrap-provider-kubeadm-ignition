df1631e37bbe180a1112f084e5a984623c810f4c
mskj-zanghao <zanghao@mskj.com>
2020-11-04 22:02:34 +0800

    add hub password

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+
 make docker-push IMG=${REPO}:${COMMIT}
